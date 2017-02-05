475b14e6a9090c5a38c9df434eab8e05485c7f81
Brendan Fortuner <bfortuner@gmail.com>
2017-02-05 10:17:17 -0800

    Completed Logistic Regression w Scikit example

--- logistic_alternative_scikit.py
+++ logistic_alternative_scikit.py
@@ -1,37 +1,47 @@
 import math
 import numpy as np
 import pandas as pd
-from pandas import DataFrame
-from sklearn import preprocessing
-from sklearn.linear_model import LogisticRegression
-from sklearn.cross_validation import train_test_split
-from numpy import loadtxt, where
-from pylab import scatter, show, legend, xlabel, ylabel
+import sklearn
+import pylab
 
-min_max_scaler = preprocessing.MinMaxScaler(feature_range=(-1,1))
-df = pd.read_csv(DATA_DIR+"logistic3.csv", header=0)
+#Source: https://github.com/perborgen/LogisticRegression/blob/master/logistic.py
+
+#min_max_scaler = preprocessing.MinMaxScaler(feature_range=(-1,1))
 
 
 # clean up data
+df = pd.read_csv(DATA_DIR+"logistic3.csv", usecols=[0,1,2])
+df.head()
+
 df.columns = ["grade1","grade2","label"]
+df.head()
 
-x = df["label"].map(lambda x: float(x.rstrip(';')))
+df['label'] = df.label.apply(lambda x: float(x.rstrip(';')))
 
+df.head()
 
+# Normalize grades to values between 0 and 1 for more efficient computation
+min_max_scaler = sklearn.preprocessing.MinMaxScaler(feature_range=(-1,1))
+
+# Extract Features + Labels
 X = df[["grade1","grade2"]]
 X = np.array(X)
 X = min_max_scaler.fit_transform(X)
-Y = df["label"].map(lambda x: float(x.rstrip(';')))
+Y = df["label"]
 Y = np.array(Y)
 
+print X.shape
+print Y.shape
+print X[:5]
+print Y[:5]
 
-# if want to create a new clean dataset
-#X = pd.DataFrame.from_records(X,columns=['grade1','grade2'])
-#X.insert(2,'label',Y)
-#X.to_csv('data2.csv')
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import train_test_split
 
 # creating testing and training set
 X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.33)
+#print X_train,X_test
+#print Y_train,Y_test
 
 # train scikit learn model
 clf = LogisticRegression()
@@ -39,14 +49,27 @@ clf.fit(X_train,Y_train)
 print 'score Scikit learn: ', clf.score(X_test,Y_test)
 
 # visualize data, uncomment "show()" to run it
-pos = where(Y == 1)
-neg = where(Y == 0)
-scatter(X[pos, 0], X[pos, 1], marker='o', c='b')
-scatter(X[neg, 0], X[neg, 1], marker='x', c='r')
-xlabel('Exam 1 score')
-ylabel('Exam 2 score')
-legend(['Not Admitted', 'Admitted'])
-#show()
+pos = np.where(Y == 1)
+neg = np.where(Y == 0)
+pylab.scatter(X[pos, 0], X[pos, 1], marker='o', c='b')
+pylab.scatter(X[neg, 0], X[neg, 1], marker='x', c='r')
+pylab.xlabel('Exam 1 score')
+pylab.ylabel('Exam 2 score')
+pylab.legend(['Not Admitted', 'Admitted'])
+pylab.show()
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import train_test_split
+
+# creating testing and training set
+X_train,X_test,Y_train,Y_test = train_test_split(X,Y,test_size=0.33)
+#print X_train,X_test
+#print Y_train,Y_test
+
+# train scikit learn model
+clf = LogisticRegression()
+clf.fit(X_train,Y_train)
+print 'score Scikit learn: ', clf.score(X_test,Y_test)
 
 def Sigmoid(z):
     G_of_Z = float(1.0 / float((1.0 + math.exp(-1.0*z))))
@@ -104,7 +127,6 @@ def Logistic_Regression(X,Y,alpha,theta,num_iters):
             Cost_Function(X,Y,theta,m)
             print 'theta ', theta
             print 'cost is ', Cost_Function(X,Y,theta,m)
-Declare_Winner(theta)
 
 def Declare_Winner(theta):
     score = 0
@@ -126,8 +148,11 @@ def Declare_Winner(theta):
         print 'Your score: ', my_score
     print 'Scikits score: ', scikit_score
 
+
 # setting variables
 initial_theta = [0,0]
 alpha = 0.1
 iterations = 1000
-#Logistic_Regression(X,Y,alpha,initial_theta,iterations)
+Declare_Winner(initial_theta)
+Logistic_Regression(X,Y,alpha,initial_theta,iterations)
+
